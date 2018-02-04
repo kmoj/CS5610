9f17128b95b9ad26a9059dcb0a368ed6d8dcd06c
teng <zhang.ten@husky.neu.edu>
2018-02-04 17:31:58 -0500

    add tests and comments

--- hw04/calc/lib/calc.ex
+++ hw04/calc/lib/calc.ex
@@ -17,22 +17,40 @@ defmodule Calc do
   end
 
 
+  #define struct exp
+  #with four fields
+  #int  integer   the opperands on ther right hand side
+  #acc  integer   the results of math expression that has been evalued
+  #expr list      the list of number and math op waiting to be evalued
+  #error string   if there's any error in the math expression the error
+  #massage is put in here
+
+  #GIVEN: String
+  #RETURN: the math evaluation result of the given str
   def eval(str) do
     str = String.trim(str)
     if String.length(str) == 0 || String.length(str) == 1 do
       str
     else
-      exp = %{:int => nil, :acc => nil, :expr => syntax_process(str), :error => nil}
+
+      exp = %{:int => nil, 
+              :acc => nil, 
+              :expr => syntax_process(str), 
+              :error => nil
+            }
       final_result = add_minus(exp)
-      #IO.inspect(final_result[:error])
       if final_result[:error] == nil do
         final_result[:acc]
       else
         final_result[:error]
       end
+
     end
   end
 
+  #GIVEN: a string
+  #RETURN: a list with all the number and math opperator
+  #appeared in the given string as an item
   def syntax_process(str)  do
     str
     |>String.splitter("")
@@ -42,6 +60,10 @@ defmodule Calc do
     |>String.split()
   end
 
+  #GIVEN: a string which is either "+", "-", "*"
+  #"/", "(", ")"
+  #RETURN: a string like the given one with space 
+  #concat to its both side 
   def insert_space(x) do
     if x == "+" || x == "-" || x == "*" || x == "/" || x == "(" || x == ")" do
       " "<>x<>" "
@@ -50,95 +72,82 @@ defmodule Calc do
     end
   end
 
+  #GIVEN: a exp
+  #RETURN: the result from the math evaluation of it
   def add_minus(exp) do
     exp
     |>multi_div()
     |>add_minus_helper()
-    #multi_div_result = multi_div(exp)
-    # #IO.puts("add_minus: " )
-    # #IO.inspect(multi_div_result[:expr])
-    #add_minus_helper(multi_div_result)
   end
 
+  #GIVEN: a exp
+  #RETURN: the result from the math evaluation of it
+  #WHERE: the the next math opperation going to do is either + or -
   def add_minus_helper(multi_div_result) do
-    # #IO.puts("add_minus: " )
-    # #IO.inspect(exp[:expr])
-    # cond do
-    #   Enum.count(exp[:expr]) == 0 -> exp
-    #   #Enum.count(exp[:expr]) == 1 -> %{:acc => String.to_float(List.first(exp[:expr])), :expr => exp[:expr]}
-    #   true -> result = multi_div(exp)
-    # end
       lhs = multi_div_result[:acc]
-      #IO.puts("add_minus_helper")
-      #IO.puts(multi_div_result[:expr])
-      #IO.puts("add_minus_helper ")
-      #IO.puts(Integer.to_string(lhs))
       op = List.first(multi_div_result[:expr])
       tail = Enum.take(multi_div_result[:expr], 1 - Enum.count(multi_div_result[:expr]))
+
       if op == "+" || op == "-" do
-        new_exp = %{:acc => nil, :int => nil, :expr => tail, :error => multi_div_result[:error]}
+        new_exp = %{:acc => nil, 
+                    :int => nil, 
+                    :expr => tail, 
+                    :error => multi_div_result[:error]
+                  }
         rhs_exp = multi_div(new_exp)
-        #IO.puts("add_minus_helper find rhs")
         rhs = rhs_exp[:acc]
-        #IO.puts(rhs_exp[:expr])
-        # if rhs_exp[:expr] == nil do
-        #   tail == []
-        # else
-        #   tail = Enum.take(rhs_exp[:expr], 1 - Enum.count(rhs_exp[:expr]))
-        # end
 
-      if rhs == nil do
-        rhs = 1
-        error_flag = "Error: rhs is missing"
-      else
-        error_flag = rhs_exp[:error]
-      end
+        if rhs == nil do
+          rhs = 1
+          error_flag = "Error: rhs is missing"
+        else
+          error_flag = rhs_exp[:error]
+        end
 
-       if op == "+" do
-        #IO.puts("add_minus_helper rhs in *")
-        #IO.inspect(rhs)
-          calculate_result = %{:acc => lhs + rhs, :int => rhs_exp[:int], :expr => rhs_exp[:expr], :error => error_flag}
-       else
-          calculate_result = %{:acc => lhs - rhs, :int => rhs_exp[:int], :expr => rhs_exp[:expr], :error => error_flag}
-       end
-       #IO.puts(" add_minus_helper 111")
-       #IO.puts(calculate_result[:expr])
-       add_minus_helper(calculate_result)
+        if op == "+" do
+          calculate_result = %{:acc => lhs + rhs, 
+                               :int => rhs_exp[:int], 
+                               :expr => rhs_exp[:expr], 
+                               :error => error_flag
+                             }
+        else
+          calculate_result = %{:acc => lhs - rhs, 
+                               :int => rhs_exp[:int], 
+                               :expr => rhs_exp[:expr], 
+                               :error => error_flag
+                             }
+        end
+        add_minus_helper(calculate_result)
+
      else
       multi_div_result
     end
   end
   
+  #GIVEN: a exp
+  #RETURN: the result from the math evaluation of it
   def multi_div(exp) do
     exp
     |>factor()
     |>multi_div_helper()
-    # #IO.puts("enter multi_div: ")
-    # #IO.inspect(exp[:acc])
-    # #IO.inspect(exp[:expr])
-    # factor_result = factor(exp)
-    # multi_div_helper(factor_result)
   end
 
+  #GIVEN: a exp
+  #RETURN: the result from the math evaluation of it
+  #WHERE: the the next math opperation going to do is either * or /
   def multi_div_helper(factor_result) do
       lhs = factor_result[:acc]
-      #IO.puts("multi_div_helper")
-      #IO.puts(factor_result[:expr])
-      #IO.puts("multi_div_helper2 ")
-      #IO.inspect(lhs)
       op = List.first(factor_result[:expr])
       tail = Enum.take(factor_result[:expr], 1 - Enum.count(factor_result[:expr]))
       if op == "*" || op == "/" do
-        new_exp = %{:acc => factor_result[:acc], :int => factor_result[:int], :expr => tail, :error => factor_result[:error]}
+
+        new_exp = %{:acc => factor_result[:acc], 
+                    :int => factor_result[:int], 
+                    :expr => tail, 
+                    :error => factor_result[:error]
+                  }
         rhs_exp = factor(new_exp)
-        #IO.puts("find rhs")
         rhs = rhs_exp[:int]
-        #IO.puts(rhs_exp[:expr])
-        # if rhs_exp[:expr] == nil do
-        #   tail == []
-        # else
-        #   tail = Enum.take(rhs_exp[:expr], 1 - Enum.count(rhs_exp[:expr]))
-        # end
         error_flag = rhs_exp[:error]
         if rhs == nil do
           rhs = 0
@@ -146,9 +155,11 @@ defmodule Calc do
         end
 
        if op == "*" do
-        #IO.puts("rhs in *")
-        #IO.inspect(rhs)
-          calculate_result = %{:acc => lhs * rhs, :int => rhs_exp[:int], :expr => rhs_exp[:expr], :error => error_flag}
+          calculate_result = %{:acc => lhs * rhs, 
+                               :int => rhs_exp[:int], 
+                               :expr => rhs_exp[:expr], 
+                               :error => error_flag
+                             }
        else
           if rhs == 0 || rhs == nil do
             rhs = 1
@@ -156,49 +167,62 @@ defmodule Calc do
           else
             error_flag = rhs_exp[:error]
           end
-          calculate_result = %{:acc => div(lhs, rhs), :int => rhs_exp[:int], :expr => rhs_exp[:expr], :error => error_flag}
+          calculate_result = %{:acc => div(lhs, rhs), 
+                               :int => rhs_exp[:int], 
+                               :expr => rhs_exp[:expr], 
+                               :error => error_flag
+                             }
        end
-       #IO.puts("111")
-       #IO.puts(calculate_result[:expr])
        multi_div_helper(calculate_result)
+
      else
       factor_result
     end
   end
 
+  #GIVEN: a exp
+  #RETURN: the result from the math evaluation of it
+  #WHERE: the first item on the exp's expr field is 
+  #either ( or is a integer string
   def factor(exp) do
-    #IO.puts("factor: " )
-    #IO.inspect(exp[:expr])
     if exp[:expr] == nil ||  List.first(exp[:expr]) == nil do
-      %{:acc => exp[:acc], :int => nil, :expr => exp[:expr], :error => exp[:error]}
+
+      %{:acc => exp[:acc], 
+        :int => nil, 
+        :expr => exp[:expr], 
+        :error => exp[:error]
+      }
+
     else
       
       head = List.first(exp[:expr])
       tail = Enum.take(exp[:expr], 1 - Enum.count(exp[:expr]))
 
         if head == "(" do
-          new_exp = %{:acc => nil, :int => nil, :expr => tail, :error => exp[:error]}
+
+          new_exp = %{:acc => nil, 
+                      :int => nil, 
+                      :expr => tail, 
+                      :error => exp[:error]
+                    }
           temp_result = add_minus(new_exp)
           if temp_result[:expr] == nil do
             tail = []
-            #IO.puts("nil" )
           else
             tail = Enum.take(temp_result[:expr], 1 - Enum.count(temp_result[:expr]))
-            #IO.puts("1" )
           end
-          #IO.puts("factor2: " )
-          #IO.inspect(tail)
-          #IO.puts("factor after ) acc value 1: " )
-          #IO.inspect(temp_result[:acc])
-          #IO.puts("factor after ) int value 1: " )
-          #IO.inspect(temp_result[:int])
-          #IO.puts("factor after ) int value 2: " )
-          #IO.inspect(exp[:int])
-
           if exp[:acc] == nil do
-            %{:acc => temp_result[:acc], :int => temp_result[:acc], :expr => tail, :error => temp_result[:error]}
+            %{:acc => temp_result[:acc], 
+              :int => temp_result[:acc], 
+              :expr => tail, 
+              :error => temp_result[:error]
+            }
           else
-            %{:acc => exp[:acc], :int => temp_result[:acc], :expr => tail, :error => temp_result[:error]}
+            %{:acc => exp[:acc], 
+              :int => temp_result[:acc], 
+              :expr => tail, 
+              :error => temp_result[:error]
+            }
           end
           
         else
@@ -212,26 +236,25 @@ defmodule Calc do
           end
 
           if exp[:acc] == nil do
-            #IO.puts(":acc nil " )
             %{:acc => num, :int => num, :expr => tail, :error => error_flag}
           else
-            #IO.puts(":acc: " )
-            #IO.puts(Integer.to_string(exp[:acc]))
-            #IO.puts(":int: " )
-            #IO.puts(head)
             %{:acc => exp[:acc], :int => num, :expr => tail, :error => error_flag}
           end
         end
     end
   end
 
+  #GIVEN: a string
+  #RETURN: boolean
+  #ture if the string is only contain char digits
   def is_integer?(arg) do
-    
     list =  String.splitter(arg, "", trim: true)
-    ans = Enum.all?(list, fn(x) -> is_digit?(x) end)
-    #Enum.all?(list, fn(x) -> is_digit?(x) end)
+    Enum.all?(list, fn(x) -> is_digit?(x) end)
   end
 
+  #GIVEN: a string of length 1
+  #RETURN: boolean
+  #ture if the string is char digits
   def is_digit?(arg) do
     Enum.any?(["0","1","2","3","4","5","6","7","8","9"], fn(x) -> x == arg end)
   end
--- hw04/calc/test/calc_test.exs
+++ hw04/calc/test/calc_test.exs
@@ -3,7 +3,7 @@ defmodule CalcTest do
   doctest Calc
 
   test "eval function" do
-  	#assert Calc.eval("") == ""
+  	assert Calc.eval("") == ""
   	assert Calc.eval("1") == "1"
     assert Calc.eval("+") == "+"
     assert Calc.eval("1 +") == "Error: rhs is missing"
@@ -14,5 +14,73 @@ defmodule CalcTest do
     assert Calc.eval("6*2") == 12
     assert Calc.eval("6*(2+1)") == 18
     assert Calc.eval("(6/2-1)*(2+1)") == 6
+    assert Calc.eval("(6-4/2-1)*(2+1)/3-6") == -3
   end
+
+  test "syntax_process function" do
+  	assert Calc.syntax_process("") == []
+  	assert Calc.syntax_process("1") == ["1"]
+    assert Calc.syntax_process("1 +") == ["1", "+"]
+    assert Calc.syntax_process("1.2 + 2") == ["1.2", "+", "2"]
+    assert Calc.syntax_process("6 /0") == ["6", "/", "0"]
+    assert Calc.syntax_process("6*2") == ["6", "*", "2"]
+    assert Calc.syntax_process("6*(2+1)") == ["6", "*","(", "2", "+", "1", ")"]
+  end
+
+  test "insert_space function" do
+  	assert Calc.insert_space("") == ""
+  	assert Calc.insert_space("1") == "1"
+    assert Calc.insert_space("+") == " + "
+    assert Calc.insert_space("(") == " ( "
+    assert Calc.insert_space("1.2") == "1.2"
+  end
+
+  test "add_minus function" do
+  	assert Calc.add_minus(%{:int => nil, :acc => nil, :expr => ["1", "+"], :error => nil}) 
+  	== %{:int => nil, :acc => 2, :expr => [], :error => "Error: rhs is missing"}
+  	assert Calc.add_minus(%{:int => nil, :acc => nil, :expr => ["1.2", "+", "2"], :error => nil}) 
+  	== %{:int => 2, :acc => 3, :expr => [], :error => "Error: opperator error"}
+    assert Calc.add_minus(%{:int => nil, :acc => nil, :expr => ["6", "*","(", "2", "+", "1", ")"], :error => nil}) 
+    == %{:int => 3, :acc => 18, :expr => [], :error => nil}
+  end
+
+  test "add_minus_helper function" do
+  	assert Calc.add_minus_helper(%{:int => 1, :acc => 1, :expr => ["+"], :error => nil}) 
+  	== %{:int => nil, :acc => 2, :expr => [], :error => "Error: rhs is missing"}
+    assert Calc.add_minus_helper(%{:int => 2, :acc => 6, :expr => ["+", "1"], :error => nil}) 
+    == %{:int => 1, :acc => 7, :expr => [], :error => nil}
+  end
+
+  test "multi_div function" do
+  	assert Calc.multi_div(%{:int => nil, :acc => nil, :expr => ["1", "*"], :error => nil}) 
+  	== %{:int => nil, :acc => 0, :expr => [], :error => "Error: rhs is missing"}
+  	assert Calc.multi_div(%{:int => nil, :acc => nil, :expr => ["1.2", "*", "2"], :error => nil}) 
+  	== %{:int => 2, :acc => 2, :expr => [], :error => "Error: opperator error"}
+    assert Calc.multi_div(%{:int => nil, :acc => nil, :expr => ["6", "*","(", "2", "+", "1", ")"], :error => nil}) 
+    == %{:int => 3, :acc => 18, :expr => [], :error => nil}
+  end
+
+  test "factor function" do
+  	assert Calc.factor(%{:int => nil, :acc => nil, :expr => ["1.2", "*", "2"], :error => nil}) 
+  	== %{:int => 1, :acc => 1, :expr => ["*", "2"], :error => "Error: opperator error"}
+    assert Calc.factor(%{:int => nil, :acc => nil, :expr => ["2", "*", "5"], :error => nil}) 
+    == %{:int => 2, :acc => 2, :expr => ["*", "5"], :error => nil}
+    assert Calc.factor(%{:int => nil, :acc => 3, :expr => ["2", "*", "5"], :error => nil}) 
+    == %{:int => 2, :acc => 3, :expr => ["*", "5"], :error => nil}
+    assert Calc.factor(%{:int => nil, :acc => nil, :expr => ["(", "2", "+", "5", ")", "*", "2"], :error => nil}) 
+    == %{:int => 7, :acc => 7, :expr => ["*", "2"], :error => nil}
+  end
+
+  test "is_integer? function" do
+  	assert Calc.is_integer?("123") == true
+    assert Calc.is_integer?("1.2") == false
+    assert Calc.is_integer?("+") == false
+  end
+
+  test "is_digit? function" do
+  	assert Calc.is_integer?("1") == true
+    assert Calc.is_integer?(".") == false
+    assert Calc.is_integer?("+") == false
+  end
+
 end
