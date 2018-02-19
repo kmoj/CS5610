de3e9f9d8a6123cb1df39e51915657cec74a12c4
kmoj <100032424@qq.com>
2018-02-19 22:02:45 +0000

    update

--- hw06/task_tracker/lib/task_tracker_web/router.ex
+++ hw06/task_tracker/lib/task_tracker_web/router.ex
@@ -2,23 +2,13 @@ defmodule TaskTrackerWeb.Router do
   use TaskTrackerWeb, :router
 
   pipeline :browser do
+    import TaskTrackerWeb.Plugs
     plug :accepts, ["html"]
     plug :fetch_session
     plug :get_current_user
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
-  end
-
-  def get_current_user(conn, _param) do
-    user_id = get_session(conn, :user_id)
-    if user_id do
-      user = TaskTracker.Accounts.get_user!(user_id)
-      assign(conn, :current_user, user)
-    else
-
-      assign(conn, :current_user, nil)
-    end
   end
 
   pipeline :api do
